43515ad9d9746dccbc24d2870da2317b8acbe2db
LeoRoma <xiajtn@gmail.com>
2020-07-06 21:48:36 +0100

    Leo added his script

---
+++ filmsmanager.ps1
@@ -0,0 +1,30 @@
+﻿##**$films = [System.Collections.ArrayList] @("Batman", "Spawn")**
+$films = Get-Content C:\Users\xiajt\Desktop\FilmList.txt
+function global:addfilm($filmname){
+    foreach($film in $films){
+        if($films.Contains($filmname)){
+            Write-Warning("film Already exists")
+            break
+        }else{
+            Add-Content C:\Users\xiajt\Desktop\FilmList.txt $filmname
+            break
+        }
+    }
+    
+}
+addfilm $filmname
+
+function global:getfilms{
+    foreach($film in $films){
+        $film
+    }
+}
+getfilms
+
+
+
+function global:deletefilm($filmname){
+    
+}
+deletefilm $filmname
+
